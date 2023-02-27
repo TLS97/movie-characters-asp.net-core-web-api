49185a420682c1dfdb3403b7dc345e6ebb43c159
Tine-Lovise Storvoll <tinelovisestorvoll@gmail.com>
2023-02-27 19:11:13 +0100

    feat: create data models and DbContext, generate migration and update the database

---
+++ MovieCharactersAPI/Models/Character.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MovieCharactersAPI.Models
+{
+    [Table("Character")]
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Alias { get; set; } = null!;
+        public string Gender { get; set; } = null!;
+        public string Picture { get; set; } = null!;
+
+        // Relationship
+        public int MovieId { get; set; }
+
+        // Navigation property
+        public Movie? Movie { get; set; }
+    }
+}
