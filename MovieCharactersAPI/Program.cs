7562c3b08cf001b751645f8edd9c9c2da6af8551
Tine-Lovise Storvoll <tinelovisestorvoll@gmail.com>
2023-03-03 18:21:50 +0100

    docs: fixed swagger docs

--- MovieCharactersAPI/Program.cs
+++ MovieCharactersAPI/Program.cs
@@ -1,8 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using MovieCharactersAPI.Models;
 using MovieCharactersAPI.Services.CharacterServices;
 using MovieCharactersAPI.Services.FranchiseServices;
 using MovieCharactersAPI.Services.MovieServices;
+using System.Reflection;
 
 namespace MovieCharactersAPI
 {
@@ -11,6 +14,11 @@ namespace MovieCharactersAPI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            // Set the comments path for the Swagger JSON and UI
+
+            //var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            //var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
 
             // Add services to the container.
 
@@ -26,7 +34,17 @@ namespace MovieCharactersAPI
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "MovieCharacters API",
+                    Description = "An ASP.NET Core Web API for managing franchises, movies and related characters.",
+                });
+                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+            });
 
             // Automapper
             builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
